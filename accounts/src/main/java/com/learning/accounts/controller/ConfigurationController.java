efb03808d5f2089fa8ddf8a96e464b48b1b167ae
Sanket Vyawahare <thundercapital.info@gmail.com>
2025-01-15 23:20:38 +0530

    Runtime Configuration change using /actuator/refresh

--- accounts/src/main/java/com/learning/accounts/controller/ConfigurationController.java
+++ accounts/src/main/java/com/learning/accounts/controller/ConfigurationController.java
@@ -2,6 +2,7 @@ package com.learning.accounts.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/config")
+@RefreshScope
 public class ConfigurationController {
 
     @Value("${root.name}")
@@ -35,4 +37,19 @@ public class ConfigurationController {
         return game;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
 }
