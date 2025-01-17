0b580c63dec13d29e8a09cbd4fa9c6a1d162a643
Sanket Vyawahare <thundercapital.info@gmail.com>
2025-01-17 20:37:50 +0530

    Added Gateway Server support for central entry point

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ This repository contains the coursework done for the Microservices course.
 ## OpenAPI Documentation 
 
 Add dependency for springdoc. The documentation will be exposed on below path - `http://localhost:8081/swagger-ui/index.html`
+
+## Reactive Gateway
+
+`http://172.27.240.1:8051/actuator/gateway/routes` -> to view all the routes
---
+++ api-gateway/src/main/java/com/learning/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,34 @@
+package com.learning.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route(p-> p
+                    .path("/api/account/**")
+                    .filters(f -> f.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                            .addRequestHeader("X-Correlation-Id", "ABED"))
+                    .uri("lb://ACCOUNTS")
+                    )
+                .route(p-> p
+                    .path("/api/loans/**")
+                    .uri("lb://LOANS")
+                ).build();
+    }
+}
