b08a497240488a3bc61d233752bd055c9c17d8cc
Sanket Vyawahare <thundercapital.info@gmail.com>
2025-01-25 22:56:16 +0530

    Added Parameterized Testing

--- testing-demo-java/src/test/java/com/learning/AppTest.java
+++ testing-demo-java/src/test/java/com/learning/AppTest.java
@@ -4,16 +4,30 @@ package com.learning;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 @DisplayName("App Class Tests")
 class AppTest {
 
-    @Test
     @DisplayName("Addition of 3+2")
-    void additionDemo2and3(){
+    @ParameterizedTest
+    @MethodSource("additionparameter")
+    void additionDemo(int a, int b, int expectedResult){
         App myapp = new App();
-        int result = myapp.additionMethod(2,3);
-        assertEquals(5,result,"Addition of 2 & 3 is 5");
+        int result = myapp.additionMethod(a,b);
+        assertEquals(expectedResult,result,"Addition of 2 & 3 is 5");
+    }
+
+    private static Stream<Arguments> additionparameter(){
+        return Stream.of(
+                Arguments.of(10,20, 30),
+                Arguments.of(10,-20,-10),
+                Arguments.of(50,20,70)
+        );
     }
 
     @Test
